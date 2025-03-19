2ff431559d2e07774d7759f0a2bf30f1e5aa0a34
Flávio Silva <73346782+FuzzyLaDuzzy@users.noreply.github.com>
2025-03-19 17:31:57 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -137,8 +137,7 @@ export default function Home() {
   const isTouchDevice = () => {
     return (
       "ontouchstart" in window ||
-      navigator.maxTouchPoints > 0 ||
-      navigator.msMaxTouchPoints > 0
+      navigator.maxTouchPoints > 0
     );
   };
 
